903bb47a4643518541dd3e335650521b71a63b36
Abhishek Prajapati <bhishekprajapati@gmail.com>
2025-01-30 09:03:56 +0530

    fix: key warnings

--- app/(routes)/design/_components/presence.tsx
+++ app/(routes)/design/_components/presence.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Fragment, useEffect, useMemo } from "react";
+import { nanoid } from "nanoid";
+import { Fragment, useEffect, useMemo, useRef } from "react";
 import { useOthers, useSelf, useUpdateMyPresence } from "@liveblocks/react";
 import { useCanvas } from "@/hooks/use-fabric";
 import Cursor from "@/components/cursor";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { cn } from "@/lib/utils";
 import {
   Tooltip,
   TooltipContent,
@@ -13,9 +13,13 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
+const randPresenceId = () => nanoid(5);
+
 export const PresenceCursors = () => {
   const canvas = useCanvas();
   const update = useUpdateMyPresence();
+  const RAND_ID_REF = useRef(randPresenceId());
+
   const COLORS = [
     "#E57373",
     "#9575CD",
@@ -36,6 +40,7 @@ export const PresenceCursors = () => {
           x: scenePoint.x,
           y: scenePoint.y,
         },
+        randId: RAND_ID_REF.current,
       })
     );
   }, [canvas]);
@@ -67,18 +72,22 @@ export const PresenceAvatars = () => {
   const others = useOthers();
   const self = useSelf();
 
-  const users = useMemo(() => [self, ...others], [self, others]);
-  const classes = cn("flex items-center");
+  const users = useMemo(
+    () => [self, ...others].filter((user) => user !== null),
+    [self, others]
+  );
 
   return (
-    <ul className={classes}>
+    <ul className="flex items-center">
       {users.map((user, idx) => {
-        if (!user) return <></>;
-        const { id, info } = user;
+        const { id, info, presence } = user;
+        const randId =
+          presence.randId === undefined ? randPresenceId() : presence.randId;
+
         return (
           <li
             className="relative"
-            key={id}
+            key={`${id}-${randId}`}
             style={{
               transform: `translateX(-${idx / 2}rem)`,
               zIndex: users.length - idx,
