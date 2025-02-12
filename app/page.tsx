2b4814229a4b4d855e77b5733986fd441776cdaf
Abhishek Prajapati <bhishekprajapati@gmail.com>
2025-02-12 11:01:42 +0530

    fix: index page styles

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex h-dvh items-center justify-center">
       <SignedOut>
-        <SignInButton />
+        <Button>
+          <SignInButton forceRedirectUrl={"/files"} />
+        </Button>
       </SignedOut>
       <SignedIn>
         <Button>
           <Link href="/files">My Files</Link>
         </Button>
       </SignedIn>
-    </>
+    </div>
   );
 }
